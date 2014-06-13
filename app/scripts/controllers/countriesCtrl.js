9e4435f124e3fcfa95487d6cd010a87c6f9918d0
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-06-13 15:58:48 -0700

    country commits

--- app/scripts/controllers/countriesCtrl.js
+++ app/scripts/controllers/countriesCtrl.js
@@ -1,20 +1,39 @@
 'use strict';
 
-window.app.controller('countriesCtrl', ['$scope', '$routeParams', 'countrySingle',
-  function($scope, $routeParams, countrySingle) {
+window.app.controller('countriesCtrl', ['$scope', '$routeParams', 'countrySingle','countryCity', 'countryNeighbor',
+  function($scope, $routeParams, countrySingle, countryCity, countryNeighbor) {
     
     $scope.cache = [];
     $scope.data = {};
+    $scope.capital;
     $scope.cName = $routeParams.country;
-
 
     countrySingle.load($scope.cName).then(function(data){
         $scope.data = data['geonames'][0];
-        console.log($scope.data);
           if($scope.data === undefined)
               return;
           
           $scope.cache.push($scope.data);
 
     });
+    countryCity.load($scope.cName).then(function(data){
+        $scope.lol = data['geonames'][0];
+          if($scope.lol === undefined)
+              return;
+          
+          $scope.cache.push($scope.lol);
+
+    });
+
+    countryNeighbor.load($scope.cName).then(function(data){
+        $scope.neighbor1 = data['geonames'][0];
+        $scope.neighbor2 = data['geonames'][1];
+        $scope.neighbor3 = data['geonames'][2];
+          if($scope.neighbor === undefined)
+              return;
+          
+          $scope.cache.push($scope.neighbor);
+
+    });    
+
   }]);
