d522059eebfcc907f4234ba177f8438264722b19
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-06-13 14:21:10 -0700

    commits

---
+++ scripts/controllers/capitalCtrl.js
@@ -0,0 +1,5 @@
+window.app.controller('capitalCtrl', ['$scope', 'capital', function($scope, capital){
+
+	$scope.capital = capital;
+	
+}])
---
+++ scripts/controllers/main.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+window.app.controller('MainCtrl', ['$scope', 'countryService',
+  				 function ($scope, countryService) {
+  				 	$scope.cache = [];
+  				 	$scope.data = {};
+  				 	countryService.load().then(function(data){
+      						$scope.data = data['geonames'];
+      						console.log($scope.data);
+						      if($scope.data === undefined)
+						          return;
+						      
+						      $scope.cache.push($scope.data);
+
+						});
+				
+  }]);
+
+
